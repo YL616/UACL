7a2dbdab57bea422cb67f48c2b5f07e09dff69a1
Yl <142292144+YL616@users.noreply.github.com>
2024-09-26 16:53:14 +0800

    Update network.py

--- UACL/modules/network.py
+++ UACL/modules/network.py
@@ -9,8 +9,8 @@ class Network(nn.Module):
         self.resnet = resnet
         self.feature_dim = feature_dim
         self.cluster_num = class_num
-#nn.Sequential是用于构建神经网络的PyTorch模块，允许用户按照顺序将多个层组合在一起，以构建一个神经网络模型
-        # fc_layer = nn.Linear(self.resnet.rep_dim, self.resnet.rep_dim)   # 使用同一个层，共享权重之后反而效果很差（C3就已经很差了）
+
+        # fc_layer = nn.Linear(self.resnet.rep_dim, self.resnet.rep_dim)   
         self.instance_projector = nn.Sequential(
             # nn.Linear(self.resnet.rep_dim, self.resnet.rep_dim),  # Linear(in_features=512, out_features=512, bias=True)
             nn.Linear(self.resnet.rep_dim, self.resnet.rep_dim),
@@ -28,11 +28,11 @@ class Network(nn.Module):
 
     def forward(self, x_i, x_j):
         h_i = self.resnet(x_i)
-        h_j = self.resnet(x_j)  # 得到特征提取的结果
+        h_j = self.resnet(x_j)  
         # print('repdim:', self.resnet.rep_dim)
 
         z_i = normalize(self.instance_projector(h_i), dim=1)
-        z_j = normalize(self.instance_projector(h_j), dim=1)  # 对特征提取结果进行归一化？ 对，应该是样例级别，得到所属类别可能性后，需要归一化（一张图就属于一个类）
+        z_j = normalize(self.instance_projector(h_j), dim=1) 
 
         c_i = self.cluster_projector(h_i)
         c_j = self.cluster_projector(h_j)  #
@@ -43,6 +43,5 @@ class Network(nn.Module):
     def forward_cluster(self, x):
         h = self.resnet(x)
         c = self.cluster_projector(h)
-        c = torch.argmax(c, dim=1) # torch.argmax是一个PyTorch函数，它返回给定维度上张量中最大值的索引
-        #将返回c张量中每行最大值的索引，这些索引将被用作聚类的标签。
-        return c  # c是x最有可能所属的类别？
+        c = torch.argmax(c, dim=1) 
+        return c  
